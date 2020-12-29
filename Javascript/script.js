80de6a378140a2aac40a0141780b79278907de85
Bram <bramgoudsmid@hotmail.com>
2020-12-30 00:44:40 +0100

    Portfolio About af, Home Page nog Responsive maken

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".typing",{
+    strings: ["Developer","Designer", "Broodje"],
+    typeSpeed: 10,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Developer","Designer", "Broodje"],
+    typeSpeed: 10,
+    backSpeed: 0,
+    loop: true
+});
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
