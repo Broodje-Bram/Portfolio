5e4a4a56065f8d8a31fb2a549a966c79f6dbe188
Bram <bramgoudsmid@hotmail.com>
2021-01-12 21:20:48 +0100

    Team beetje gemaakt

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,15 +4,3 @@ var typed = new Typed(".typing",{
     backSpeed: 60,
     loop: true
 });
-
-var typed = new Typed(".typing-2", {
-    strings: ["Developer","Designer", "Broodje"],
-    typeSpeed: 10,
-    backSpeed: 0,
-    loop: true
-});
-
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
